f54103756427635308114285595e5127338ab72e
Nathan W Brei <nathan.w.brei@gmail.com>
2017-10-07 00:11:29 +0200

    Helper function for creating a random blocksparse matrix

--- dense_x_blocksparse/blocksparse.c
+++ dense_x_blocksparse/blocksparse.c
@@ -1,3 +1,5 @@
+#include<stdlib.h>
+
 
 struct blocksparse {
   int rows;          // Number of 3x3 blocks
@@ -13,11 +15,46 @@ struct colmajor {
   double * values; // Size = rows*cols
 };
 
-struct blocksparse make_random_blocksparse(int rows, int cols, int pattern) {
+struct blocksparse make_random_blocksparse(int rows, int cols, int pattern, int blocksize) {
+
+  struct blocksparse result;
+  result.rows = rows;
+  result.cols = cols;
+  result.pattern = pattern;
+  result.blocksize = blocksize;
+  double * values = (double *) malloc(blocksize*rows*cols*sizeof(double));
+  result.values = values;
+
+  // Figure out nnz by counting number of 1's in pattern bitmask
+  int nnz = 0;
+  for (int k=0; k<9; k++) {
+    nnz += pattern & 1;
+    pattern = pattern >> 1;
+  }
+
+  // For each block (blockwise column-major)
+  for (int j=0; j<cols; j++) {
+    for (int i=0; i<rows; i++) {
+      for (int k=0; k<nnz; k++) {
+        values[k] = 1.0;    // Maybe make this random?
+      }
+      for (int k=nnz; k<blocksize; k++) {
+        values[k] = 1./0;   // These are only used for padding, so let's poison them
+      }
+      values += blocksize;  // Move on to the next block
+    }
+  }
+  return result;
 }
 
 struct colmajor blocksparse_to_colmajor(struct blocksparse input) {
-  
+  struct colmajor result;
+  result.rows = 3*input.rows;
+  result.cols = 3*input.cols;
+  result.values = (double *) malloc(result.rows * result.cols * sizeof(double));
+
+
+  return result;
 }
 
 
