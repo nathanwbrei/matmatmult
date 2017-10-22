622452e794bdf59853f60f6f1bf6b341708d5ccd
Nathan W Brei <nathan.w.brei@gmail.com>
2017-10-22 22:40:12 +0200

    Very beginnings of experiment generator

--- codegen/experimenter.py
+++ codegen/experimenter.py
@@ -1,13 +1,49 @@
 
-
+from harness import *
 from libxsmm_standard import *
 from ccode import *
 
-dest_filename = "../dense_x_blocksparse/gemms_dense_x_dense_py.h"
-code = make_cfunc("gemm_dense_snb_py", make_gemm(snb), True)
 
-with open(dest_filename, "w") as f:
-    f.writelines(code)
+def run():
+
+    # First test: Use libxsmm_standard 48x9x9 dense_x_sparse on SNB
+    # Vary:       Sparsity patterns
+    # Expect:     As matrix becomes sparse, mult becomes memory-bound
+    #             TODO: [Calculate roofline of IVB]
+    #             As matrix becomes dense, mult becomes instruction-cache bound
+    #             TODO: [What is instr cache size? What is instr size?]
+
+    # Ultimately: Vary fraction nonzero x matrix width x registerblocking x architecture
+
+    # Now repeat on different architectures using the same register blocking
+    # Second test: Use the 1D libxsmm register blocking. What happens now?
+
+    dest_filename = "../dense_x_blocksparse/gemms_dense_x_blocksparse.c"
+
+    with open(dest_filename, "w") as f:
+        f.write(make_exp_code(alg_builder = make_gemm,
+                              alg_namer = lambda p: f"libxsmm_{len(p.C_regs)}x{len(p.C_regs[0])}",
+                              params = [snb, hsw]))
+
+
+
+def make_exp_code(alg_builder, alg_namer, params) -> str:
+
+    result = ""
+    harness = HarnessBuilder()
+    result += harness.make_imports()
+
+    for param in params:
+        test_name = alg_namer(param)
+        test_asm = alg_builder(param)
+        test_c = make_cfunc(test_name, test_asm, True)
+        result += test_c + "\n\n"
+
+    main_code = harness.make_main()
+    result += main_code
+    return result
+
+
 
 
 
