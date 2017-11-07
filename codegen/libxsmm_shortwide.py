2ada9d02e107c3b8e34ae58ddd95927ada96b772
Nathan W Brei <nathan.w.brei@gmail.com>
2017-11-07 20:44:21 +0100

    Beginnings of libxsmm-shortwide-small generator

--- codegen/libxsmm_shortwide.py
+++ codegen/libxsmm_shortwide.py
@@ -13,59 +13,45 @@ class Parameters(NamedTuple):
     lda: int
     ldb: int
     ldc: int
-    instruction_sets: List[str]  # ["avx128", "avx256", "fma", "avx512"]
-    vector_width: int
-    C_regs: List[List[Register]]
-    stream_reg: Register
-    bcast_regs: List[Register]
-    temp_regs: List[Register]
-    scalar_bytes: int = 8
-    m_reg: Register = r(12)
-    n_reg: Register = r(13)
-    k_reg: Register = r(14)
-    A_ptr: Register = rdi
-    B_ptr: Register = rsi
-    C_ptr: Register = rdx
+    A_regs: List[Register]
+    C_regs: List[Register]
 
-    def A(self):
-        return MatrixCursor("A", self.A_ptr, self.m, self.k, self.lda,
-                            len(self.C_regs) * self.vector_width,
-                            self.k,
-                            self.scalar_bytes, self.vector_width)
+    def cursors(self):
+        scalar_bytes = 8
+        vec_width = 8
+        mb = vec_width
+        nb = len(self.C_regs)
+        kb = len(self.A_regs)
 
-    def B(self):
-        return MatrixCursor("B", self.B_ptr, self.k, self.n, self.ldb,
-                            self.k,
-                            len(self.C_regs[0]),
-                            self.scalar_bytes, self.vector_width)
-
-    def C(self):
-        return MatrixCursor("C", self.C_ptr, self.m, self.n, self.ldc,
-                            len(self.C_regs) * self.vector_width,
-                            len(self.C_regs[0]),
-                            self.scalar_bytes, self.vector_width)
+        A = MatrixCursor("A", rdi, self.m, self.k, self.lda, mb, kb, scalar_bytes, vec_width)
+        B = MatrixCursor("B", rsi, self.k, self.n, self.ldb, kb, nb, scalar_bytes, vec_width)
+        C = MatrixCursor("C", rdx, self.m, self.n, self.ldc, mb, nb, scalar_bytes, vec_width)
+        return (A,B,C)
 
 
-snb = Parameters(
-    m = 48, n = 9, k = 9, lda = 48, ldb = 9, ldc = 48,
-    instruction_sets = ["avx256"],
-    vector_width = 4,
-    C_regs = [[ymm(7), ymm(10), ymm(13)],
-              [ymm(8), ymm(11), ymm(14)],
-              [ymm(9), ymm(12), ymm(15)]],
-    stream_reg = ymm(3),
-    bcast_regs = [ymm(0), ymm(1), ymm(2)],
-    temp_regs = [ymm(4), ymm(5), ymm(6)])
+knl = Parameters(
+    m = 48, n = 9, k = 9,
+    lda = 48, ldb = 9, ldc = 48,
+    A_regs = [zmm(x) for x in range(9, 27)],  # zmm9..zmm26
+    C_regs = [zmm(x) for x in range(0, 8)]    # zmm0..zmm7
+    )
 
 
 
 def make_gemm(p:Parameters) -> AsmBlock:
 
-    m, n, k, A, B, C = p.m, p.n, p.k, p.A(), p.B(), p.C()
-    m_reg, n_reg, C_regs = p.m_reg, p.n_reg, p.C_regs
-    m_block, n_block = len(C_regs)*p.vector_width, len(C_regs[0])
+    m, n, k = p.m, p.n, p.k   # Number of cells in m,n,k -directions
+    mb = 8                    # Number of cells in one m-block
+    nb = len(self.C_regs)     # Number of cells in one n-block
+    kb = len(self.A_regs)     # Number of cells in one k-block
+    kB = k // kb              # Number of complete k-blocks
+    kr = k % kb               # Number of remaining cells in k-direction
 
-    asm = AsmBlock(f"LibXSMM-style {m}x{n}x{k} GEMM using {p.instruction_sets}").body([
+    A,B,C = p.cursors()                   # Access matrices from memory
+    A_regs, C_regs = p.A_regs, p.C_regs   # Access matrices from register blocks
+    m_reg, n_reg = r(12), r(13)           # Iteration variables
+
+    asm = AsmBlock(f"LibXSMM short-wide small on KNL for {m}x{n}x{k}").body([
         loop(n_reg, 0, n, n_block).body([
             loop(m_reg, 0, m, m_block).body([
                 C.load_register_block(C_regs),
@@ -81,6 +67,7 @@ def make_gemm(p:Parameters) -> AsmBlock:
     ])
     return asm
 
+def block_outer_prod(p):
 
 
 def do_something_wild(p):
