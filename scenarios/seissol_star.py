768ed3144905614fae3a7a73bec3ee1b2dac2e6a
Nathan W Brei <nathan.w.brei@gmail.com>
2017-11-19 16:35:13 +0100

    Added sparsity pattern and block decompositions for Star

--- scenarios/seissol_star.py
+++ scenarios/seissol_star.py
@@ -1,2 +1,57 @@
+from algorithms.parameters import Parameters
+from scipy import matrix
 
-# Coming soon!
+
+full_pattern = matrix(
+    [[0,0,0, 0,0,0, 1,0,0, 0,0,0, 0,0,0],
+     [0,0,0, 0,0,0, 0,1,0, 0,0,0, 0,0,0],
+     [0,0,0, 0,0,0, 0,0,1, 0,0,0, 0,0,0],
+
+     [0,0,0, 0,0,0, 1,1,1, 0,0,0, 0,0,0],
+     [0,0,0, 0,0,0, 0,1,1, 0,0,0, 0,0,0],
+     [0,0,0, 0,0,0, 0,0,1, 0,0,0, 0,0,0],
+
+     [1,1,1, 1,0,1, 0,0,0, 1,0,0, 1,0,1],
+     [1,1,1, 1,1,0, 0,0,0, 0,1,0, 1,1,0],
+     [1,1,1, 0,1,1, 0,0,0, 0,0,1, 0,1,1]])
+
+
+square_pattern = full_pattern[:, 0:12]
+skinny_pattern = full_pattern[:,0:3] | full_pattern[:,6:9]
+
+Z = full_pattern[0:3, 0:3]
+A = full_pattern[6:9, 0:3]
+B = full_pattern[6:9, 3:6]
+C = full_pattern[3:6, 6:9]
+D = full_pattern[0:3, 6:9]
+
+pattern_index = [Z, A, B, C, D]
+
+blocks_full = matrix(
+    [[0, 0, 4, 0, 0],
+     [0, 0, 3, 0, 0],
+     [1, 2, 0, 4, 2]])
+
+blocks_partial_3 = matrix(
+    [[0, 0, 4, 0, 0],
+     [0, 0, 1, 0, 0],
+     [1, 2, 0, 4, 2]])
+
+blocks_partial_2 = matrix(
+    [[0, 0, 4, 0, 0],
+     [0, 0, 1, 0, 0],
+     [1, 1, 0, 4, 1]])
+
+blocks_bsc = matrix(
+    [[0, 0, 1, 0, 0],
+     [0, 0, 1, 0, 0],
+     [1, 1, 0, 1, 1]])
+
+
+defaults = Parameters(name = "Star 48x9x9 ",
+                      Bm = 6,
+                      Bn = 3,
+                      blocks = blocks_full, # TODO: Vary this
+                      patterns = pattern_index)
+
+
