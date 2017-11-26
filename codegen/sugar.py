d5102b4d2d28b1de844e3266dc91e9f06b5dac90
Nathan W Brei <nathan.w.brei@gmail.com>
2017-11-26 14:38:31 +0100

    New syntactic sugar module which is decoupled from ast implementation

--- codegen/sugar.py
+++ codegen/sugar.py
@@ -1,25 +1,66 @@
-from codegen import blocks, loop
+from codegen.ast import *
+
+# Convenient statement constructors
+def add(src: Operand, dest: Operand):
+    stmt = AddStmt()
+    stmt.src = src
+    stmt.dest = dest
+    return stmt
+
+def label(name: str):
+    stmt = LabelStmt()
+    stmt.label = Label(name)
+    return stmt
+
+def fma(bcast_src: Operand, mult_src: Operand, add_dest: Operand):
+    stmt = FmaStmt()
+    stmt.bcast_src = bcast_src
+    stmt.mult_src = mult_src
+    stmt.add_dest = add_dest
+    return stmt
+
+def cmp(lhs: Operand, rhs: Operand):
+    stmt = CmpStmt()
+    stmt.lhs = lhs
+    stmt.rhs = rhs
+    return stmt
+
+def jump(label: str, backwards: bool):
+    stmt = JumpStmt()
+    stmt.label = label
+    stmt.backwards = backwards
+    return stmt
 
 
-def addq(*args):
-	pass
 
-def syscall():
-	pass
+# Fluent interface
+class BlockBuilder(Block):
 
-def vmulpd(r1,r2,r3):
-	pass
+    def __init__(self, description: str, parent: "BlockBuilder" = None):
+        self.parent = parent
+        self.comment = description
+        self.contents = []
+        self.block.description = description
+
+    def add(self, stmt: AsmStmt):
+        self.contents.append(stmt)
+        return self
+
+    def open(self, description: str):
+        b = BlockBuilder(description, parent=self)
+        self.contents.append(b)
+        return b
+
+    def close(self):
+        return self.parent
 
 
-b = AsmBlock("Do something interesting.").body([
 
-		addq(rax, 22),
-		syscall(),
+# S-expression interface
+def block(description: str, *args: List[AsmStmt]):
+    b = BlockBuilder(description)
+    b.contents = args
+    return b
 
-		loop(rdi,1,1,10).body([
-			addq(rax, 32)
-		]),
 
-		vmulpd(ymm1,ymm2,ymm1)
-	])
 
