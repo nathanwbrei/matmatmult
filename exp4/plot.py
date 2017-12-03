0aa597503c188fd35b699483ea9a8bdfbdcf0014
Nathan W Brei <nathan.w.brei@gmail.com>
2017-12-03 03:50:04 +0100

    Added gflops plot, now using tufte styling

--- exp4/plot.py
+++ exp4/plot.py
@@ -23,6 +23,13 @@ print(microsecs)
 print(baseline_nnzs)
 print(baseline_microsecs)
 
+#nnzs = nnzs[:-5]
+#microsecs = microsecs[:-5]
+baseline_microsecs_list = [baseline_microsecs for nnz in nnzs]
+baseline_gflops = [2*128*128*28*1e-3/baseline_microsecs for nnz in nnzs]
+baseline_nzgflops = [2*128*nnz*1e-3/baseline_microsecs for nnz in nnzs]
+gflops = [2*128*nnz*1e-3/time for (nnz,time) in zip(nnzs, microsecs)]
+
 from matplotlib import pyplot as plt
 plt.xlabel("Nonzeros")
 plt.ylabel("Microseconds")
@@ -31,7 +38,29 @@ plt.yticks(scipy.arange(0, max(microsecs+[baseline_microsecs]), 10))
 plt.plot(nnzs, microsecs, "-o")
 plt.axhline(y=baseline_microsecs, color="r", linestyle = '--')
 plt.savefig("exp4/fig.pdf")
-plt.show()
+
+# Now let's make it nice
+import prettyplotlib as ppl
+from prettyplotlib import brewer2mpl
+
+fig, ax = ppl.subplots(1)
+ppl.plot(ax, nnzs, baseline_microsecs_list, label="Baseline wall clock time", linewidth=0.75)
+ppl.plot(ax, nnzs, microsecs, label="Fully unrolled wall clock time", linewidth=0.75)
+ppl.plot(ax, nnzs, microsecs, linewidth=0.75)
+ppl.legend(ax)
+plt.xlabel("Nonzeros")
+plt.ylabel("Microseconds")
+plt.savefig("exp4/fig2.pdf")
+
+fig, ax = ppl.subplots(1)
+plt.xlabel("Nonzeros")
+plt.ylabel("GFlops")
+ppl.plot(ax, nnzs, baseline_gflops, label="Baseline hardware GFlops", linewidth=0.75)
+ppl.plot(ax, nnzs, baseline_nzgflops, label="Baseline nonzero GFlops", linewidth=0.75)
+ppl.plot(ax, nnzs, gflops, label="Fully unrolled GFlops", linewidth=0.75)
+ppl.legend(ax)
+plt.savefig("exp4/fig3.pdf")
+
 
 
 
